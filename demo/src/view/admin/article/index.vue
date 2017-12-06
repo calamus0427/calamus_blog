ddafa07465a6dc6c9ba334811a463a93e5f38582
calamus0427 <1211994507@qq.com>
2017-12-06 17:11:12 +0800

    文章管理，评论管理

--- demo/src/view/admin/article/index.vue
+++ demo/src/view/admin/article/index.vue
@@ -10,10 +10,22 @@
                 <el-input v-model="searchForm.tag" placeholder="请输入标签" size="small"></el-input>
             </el-form-item>
             <el-form-item label="创建时间" prop="createDate">
-                <el-input v-model="searchForm.createDate" placeholder="请输入创建时间" size="small"></el-input>
+                <el-date-picker
+                    v-model="searchForm.createDate"
+                    type="daterange"
+                    size="small"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期">
+                </el-date-picker>
             </el-form-item>
             <el-form-item label="发布时间" prop="publishDate">
-                <el-input v-model="searchForm.publishDate" placeholder="请输入发布时间" size="small"></el-input>
+                <el-date-picker
+                    v-model="searchForm.publishDate"
+                    type="daterange"
+                    size="small"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期">
+                </el-date-picker>
             </el-form-item>
             <el-form-item>
                 <el-button  type="primary" size="small"  icon="el-icon-search">查询</el-button>
@@ -25,31 +37,34 @@
 
         <!--应用按钮-->
         <el-button-group>
-            <el-button type="primary" size="small" @click="addFormVisible = true ">新增
+            <el-button type="primary" size="small" @click="handleAdd()">新增
             </el-button>
             <el-button type="warning" size="small" @click="refresh()">
             刷新
             </el-button>
         </el-button-group>
 
-        <el-table
+                <el-table
           :data="tableData3"
           style="width: 100%"
           height="480">
           <el-table-column
-              prop="name"
+              prop="cover"
               label="封面"
               fixed
               width="120">
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="title"
               label="博客标题"
               fixed
               width="120">
+              <template slot-scope="scope">
+                  <a @click="handleDetail()">{{scope.row.title}}  <i class="el-icon-view"></i></a>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="date"
+              prop="createTime"
               label="创建时间"
               width="120"
               sortable>
@@ -58,16 +73,16 @@
               prop="publishStatus"
               label="发布状态"
               width="120"
-              :filters="[{ text: '发布', value: 1 }, { text: '草稿', value: 0 }]"
+              :filters="[{ text: '已发布', value: 1 }, { text: '草稿', value: 0 }]"
               :filter-method="filterPublish"
               filter-placement="bottom-end">
               <template slot-scope="scope">
-                  <el-tag type="success" v-if="scope.row.publishStatus == 1">发布</el-tag>
+                  <el-tag type="success" v-if="scope.row.publishStatus == 1">已发布</el-tag>
                   <el-tag type="info" v-if="scope.row.publishStatus == 0">草稿</el-tag>
               </template>
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="topStatus"
               label="置顶状态"
               width="120"
               :filters="[{ text: '是', value: 1 }, { text: '否', value: 0 }]"
@@ -79,47 +94,46 @@
               </template>
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="tags"
               label="标签"
               width="120">
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="comment"
               label="内容概要"
               width="120">
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="remarks"
               label="备注"
               width="120">
             </el-table-column>
             <el-table-column
-              prop="date"
+              prop="publishTime"
               label="发布日期"
               width="150"
               sortable>
             </el-table-column>
             <el-table-column
-              prop="date"
+              prop="updateTime"
               label="最近修改日期"
               width="150"
               sortable>
             </el-table-column>
             <el-table-column label="数据统计">
               <el-table-column
-                prop="province"
+                prop="readNum"
                 label="阅读数"
-                width="120"
-                sortable>
+                width="120">
               </el-table-column>
               <el-table-column
-                prop="city"
+                prop="commentNum"
                 label="评论数"
                 width="120"
                 sortable>
               </el-table-column>
               <el-table-column
-                prop="address"
+                prop="pointNum"
                 label="点赞数"
                 width="300"
                 sortable>
@@ -127,18 +141,28 @@
             </el-table-column>
             <el-table-column
                 label="操作"
-                width="300"
+                width="400"
                 fixed="right">
                 <template slot-scope="scope">
                           <el-button-group >
-                              <el-button size="small" type="success">
-                                  <a class="el-icon-circle-check" style="color: white;"></a> 启用
-                              </el-button>
-                              <el-button size="small" type="danger">
-                                  <a class="el-icon-circle-close" style="color: white;"></a> 禁用
-                              </el-button>
-                              <el-button size="small" type="info">
+                              <el-button size="small" type="primary" @click="handleEdit(scope.row)">
                                   <a class="el-icon-edit" style="color: white;"></a> 编辑
+                              </el-button>
+                              <el-button size="small" type="danger" >
+                                  <a class="el-icon-circle-close" style="color: white;"></a> 移入回收站
+                              </el-button>
+                              <el-button v-if="scope.row.topStatus === 0 && scope.row.publishStatus === 1" size="small" type="warning">
+                                  <a class="el-icon-upload2" style="color: white;"></a> 置顶
+                              </el-button>
+                              <el-button v-if="scope.row.topStatus === 1" size="small" type="warning">
+                                  <a class="el-icon-setting" style="color: white;"></a> 取消置顶
+                              </el-button>
+
+                              <el-button v-if="scope.row.publishStatus === 0" size="small" type="success">
+                                  <a class="el-icon-upload" style="color: white;"></a> 发布
+                              </el-button>
+                              <el-button v-if="scope.row.publishStatus === 1" size="small" type="info">
+                                  <a class="el-icon-document" style="color: white;"></a> 存为草稿
                               </el-button>
                           </el-button-group>
                       </template>
@@ -153,110 +177,129 @@
               layout="total, sizes, prev, pager, next, jumper"
               :total="400">
             </el-pagination>
-
-        <el-dialog
-        title="收货地址"
-        :fullscreen="true"
-        :visible.sync="addFormVisible">
-          www
-        </el-dialog>
       </el-tab-pane>
       <el-tab-pane label="回收站">
-                <el-table
+        <el-form ref="searchForm" :inline="true" class="demo-form-inline"  :model="searchForm" >
+            <el-form-item label="标题" prop="title">
+                <el-input v-model="searchForm.title" placeholder="请输入标题" size="small"></el-input>
+            </el-form-item>
+            <el-form-item label="标签" prop="tag">
+                <el-input v-model="searchForm.tag" placeholder="请输入标签" size="small"></el-input>
+            </el-form-item>
+            <el-form-item label="创建时间" prop="createDate">
+                <el-date-picker
+                    v-model="searchForm.createDate"
+                    type="daterange"
+                    size="small"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="删除时间" prop="delDate">
+                <el-date-picker
+                    v-model="searchForm.delDate"
+                    type="daterange"
+                    size="small"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item>
+                <el-button  type="primary" size="small"  icon="el-icon-search">查询</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-button   type="primary" size="small" @click="resetForm('searchForm')" icon="el-icon-refresh">重置</el-button>
+            </el-form-item>
+        </el-form>
+        <el-table
           :data="tableData3"
           style="width: 100%"
           height="480">
           <el-table-column
-              prop="name"
+              prop="cover"
               label="封面"
               fixed
               width="120">
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="title"
               label="博客标题"
               fixed
               width="120">
+              <template slot-scope="scope">
+                  <a @click="handleDetail()">{{scope.row.title}}  <i class="el-icon-view"></i></a>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="date"
+              prop="createTime"
               label="创建时间"
-              width="120">
+              width="120"
+              sortable>
             </el-table-column>
             <el-table-column
-              prop="publishStatus"
-              label="发布状态"
-              width="120">
-              <template slot-scope="scope">
-                  <el-tag type="success" v-if="scope.row.publishStatus == 1">发布</el-tag>
-                  <el-tag type="info" v-if="scope.row.publishStatus == 0">草稿</el-tag>
-              </template>
+              prop="delTime"
+              label="删除时间"
+              width="150"
+              sortable>
             </el-table-column>
             <el-table-column
-              prop="name"
-              label="置顶状态"
-              width="120">
-              <template slot-scope="scope">
-                  <el-tag type="danger" v-if="scope.row.topStatus == 1">是</el-tag>
-                  <el-tag type="info" v-if="scope.row.topStatus == 0">否</el-tag>
-              </template>
-            </el-table-column>
-            <el-table-column
-              prop="name"
+              prop="tags"
               label="标签"
               width="120">
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="comment"
               label="内容概要"
               width="120">
             </el-table-column>
             <el-table-column
-              prop="name"
+              prop="remarks"
               label="备注"
               width="120">
             </el-table-column>
             <el-table-column
-              prop="date"
+              prop="publishTime"
               label="发布日期"
-              width="150">
+              width="150"
+              sortable>
             </el-table-column>
             <el-table-column
-              prop="date"
+              prop="updateTime"
               label="最近修改日期"
-              width="150">
+              width="150"
+              sortable>
             </el-table-column>
             <el-table-column label="数据统计">
               <el-table-column
-                prop="province"
+                prop="readNum"
                 label="阅读数"
-                width="120">
+                width="120"
+                sortable>
               </el-table-column>
               <el-table-column
-                prop="city"
+                prop="commentNum"
                 label="评论数"
-                width="120">
+                width="120"
+                sortable>
               </el-table-column>
               <el-table-column
-                prop="address"
+                prop="pointNum"
                 label="点赞数"
-                width="300">
+                width="300"
+                sortable>
               </el-table-column>
             </el-table-column>
             <el-table-column
                 label="操作"
-                width="300"
+                width="200"
                 fixed="right">
                 <template slot-scope="scope">
                           <el-button-group >
-                              <el-button size="small" type="success">
-                                  <a class="el-icon-circle-check" style="color: white;"></a> 启用
+                              <el-button size="small" type="info">
+                                  <a class="el-icon-refresh" style="color: white;"></a> 恢复
                               </el-button>
                               <el-button size="small" type="danger">
-                                  <a class="el-icon-circle-close" style="color: white;"></a> 禁用
-                              </el-button>
-                              <el-button size="small" type="info">
-                                  <a class="el-icon-edit" style="color: white;"></a> 编辑
+                                  <a class="el-icon-circle-close" style="color: white;"></a> 删除
                               </el-button>
                           </el-button-group>
                       </template>
@@ -273,7 +316,20 @@
             </el-pagination>
       </el-tab-pane>
     </el-tabs>
-
+        <el-dialog
+        title="新建博客"
+        :fullscreen="true"
+        :visible.sync="addFormVisible">
+        </el-dialog>
+         <el-dialog
+        title="查看详情"
+        :visible.sync="detailFormVisible">
+        </el-dialog>
+        <el-dialog
+        title="编辑博客"
+        :fullscreen="true"
+        :visible.sync="editFormVisible">
+        </el-dialog>
 </div>
 
 </template>
@@ -283,31 +339,58 @@
     data() {
       return {
         addFormVisible:false,
+        detailFormVisible:false,
+        editFormVisible:false,
         tableData3: [{
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333,
+          cover:'',
+          title:'文章1',
+          createTime:'2017-12-6',
+          updateTime:'2017-12-6',
+          publishTime:'2017-12-6',
           topStatus:1,
-          publishStatus:1
+          publishStatus:1,
+          tags:[1,2,3],
+          content:'简介1',
+          remarks:'备注1',
+          readNum:25,
+          commentNum:2,
+          pointNum:1,
         }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333,
+          cover:'',
+          title:'文章2',
+          createTime:'2017-12-6',
+          updateTime:'2017-12-6',
+          publishTime:'2017-12-6',
           topStatus:0,
-          publishStatus:0
+          publishStatus:0,
+          tags:[1,2,3],
+          content:'简介2',
+          remarks:'备注2',
+          readNum:25,
+          commentNum:2,
+          pointNum:1,
+        },{
+          cover:'',
+          title:'文章3',
+          createTime:'2017-12-6',
+          updateTime:'2017-12-6',
+          publishTime:'2017-12-6',
+          topStatus:0,
+          publishStatus:1,
+          tags:[1,2,3],
+          content:'简介3',
+          remarks:'备注3',
+          readNum:25,
+          commentNum:2,
+          pointNum:1,
         }],
-        currentPage4: 4,
+        currentPage4: 1,
         searchForm:{
           title:'',
           tag:'',
           createDate:'',
           publishDate:'',
+          delDate:'',
 
         }
       }
@@ -331,6 +414,32 @@
       filterPublish(value, row) {
         return row.publishStatus === value;
       },
+      /*新增 */
+      handleAdd(){
+                this.addFormVisible = true ;
+      },
+      /* 查看详情 */
+      handleDetail(row){
+          this.detailFormVisible = true ;
+      },
+      /*编辑 */
+      handleEdit(){
+          this.editFormVisible = true ;
+      },
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  a{
+    text-decoration: none;
+    color:#3c54d2;
+    cursor: pointer;
+  }
+  a:hover{
+    cursor: pointer;
+    color:aquamarine;
+
+  }
+</style>
+
