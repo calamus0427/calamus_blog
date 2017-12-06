fa81698e188a47d77dbb49af2c75f712163d3132
calamus0427 <1211994507@qq.com>
2017-12-06 17:16:21 +0800

    博客设置

--- demo/src/view/admin/setting/index.vue
+++ demo/src/view/admin/setting/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div >
-    <ul>
-      <li>更换皮肤</li>
-      <li>每页文章数</li>
-    </ul>
+  <el-tabs tab-position="left" >
+    <el-tab-pane label="基本设置">基本设置</el-tab-pane>
+    <el-tab-pane label="更换皮肤">更换皮肤</el-tab-pane>
+  </el-tabs>
   </div>
 </template>
 <script>
